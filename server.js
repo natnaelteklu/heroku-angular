b20e89956861268e4e70f190827eede7474d7b37
Natnael Teklu <NatnaelTe@HIBRETBANK.COM>
2025-02-27 11:49:14 +0300

    test

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist', 'heroku')));
+
+// Serve static files from the dist/heroku/browser directory
+app.use(express.static(path.join(__dirname, 'dist', 'heroku', 'browser')));
+
+// Handle all routes by serving the index.html from dist/heroku/browser
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'heroku', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'heroku', 'browser', 'index.html'));
 });
-app.listen(process.env.PORT || 8080);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
